59e25cd98067e0e095f6c74d7e1d94b8b250378b
IamVC <jack.linjie@gmail.com>
2026-09-14 15:42:49 +0800

    feat(ios): add SwiftUI app entry point

---
+++ ios/VCIQReader/VCIQReader/VCIQReaderApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct VCIQReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .tint(Color(red: 0.12, green: 0.38, blue: 0.28))
+        }
+    }
+}
